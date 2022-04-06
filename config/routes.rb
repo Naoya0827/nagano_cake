18ea0c85826db19fa104fe6b55c751db63f84fbe
Naoya0827 <n.freedom.yazuna@gmail.com>
2022-04-06 09:31:59 +0000

    [Add]adminitem関連追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :customers
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registration: 'admin/registrations'
+  }
+
+  get 'admin' => 'homes#top'
+  namespace :admin do
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :items, only:[:new, :create, :index, :show, :edit, :update]
+  end
+
+
 end
---
+++ db/migrate/20220403062711_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.string :delivery_postal_code
+      t.string :delivery_address
+      t.string :delivery_name
+      t.integer :postage
+      t.integer :billing_amount
+      t.integer :payment_method
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
