8e2a797ce478b2fa54b6d9e3995d989a29705884
Naoya0827 <n.freedom.yazuna@gmail.com>
2022-05-11 12:24:07 +0000

    [Add]orderレイアウト追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,6 +5,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path
   end
 
   def destroy
@@ -14,7 +17,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
-
+    @cart_item = current_customer.cart_items
+    @cart_item.destroy_all
+    redirect_to public_cart_items_path
   end
 
   def create
